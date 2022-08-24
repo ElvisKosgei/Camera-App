62058abcf3faff6883d5ab81f3be5f0788c4ebd0
ElvisKosgei <kosgeielvis06@mail.com>
2022-08-24 11:39:23 +0300

    Added some functionality

--- app/src/main/java/com/elvis/cameraapp/MainActivity.kt
+++ app/src/main/java/com/elvis/cameraapp/MainActivity.kt
@@ -16,29 +16,41 @@ import androidx.appcompat.app.AlertDialog
 class MainActivity : AppCompatActivity() {
     private val CAMERA_PERMISSION_CODE = 1000
     private val IMAGE_CAPTURED = 2022
-    private var imageUri : Uri? = null
-    private var imgView : ImageView? = null
+    private val GALLERY_PERMISSION_CODE = 3000
+    private val GALLERY_IMAGE_CODE = 1996
+    private var imageUri: Uri? = null
+    private var imgView: ImageView? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         imgView = findViewById(R.id.imgCaptured)
-        val btnOpenCamera : Button = findViewById(R.id.btnOpenCamera)
+        val btnOpenCamera: Button = findViewById(R.id.btnOpenCamera)
+        val btnOpenGallery: Button = findViewById(R.id.btnOpenGallery)
+
+        requestCameraPermission()
+        requestGalleryPermission()
 
         val permissionGranted = requestCameraPermission()
+        btnOpenGallery.setOnClickListener {
+            val galleryIntent = Intent(Intent.ACTION_PICK)
+            galleryIntent.type = "image/*"
+            startActivityForResult(galleryIntent, GALLERY_IMAGE_CODE)
+
+        }
         btnOpenCamera.setOnClickListener {
             //opening the camera if the permission has been granted
             //if (permissionGranted) {
-                //Open the camera
-                val values = ContentValues()
-                values.put(MediaStore.Images.Media.TITLE, "My Image")
-                values.put(MediaStore.Images.Media.DESCRIPTION, "Taken from my app")
-                imageUri = contentResolver.insert(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, values)
+            //Open the camera
+            val values = ContentValues()
+            values.put(MediaStore.Images.Media.TITLE, "My Image")
+            values.put(MediaStore.Images.Media.DESCRIPTION, "Taken from my app")
+            imageUri = contentResolver.insert(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, values)
 
-                //Creating an intent to open the camera
+            //Creating an intent to open the camera
 
-                val intentToOpenCamera = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
-                intentToOpenCamera.putExtra(MediaStore.EXTRA_OUTPUT, imageUri)
-                startActivityForResult(intentToOpenCamera, IMAGE_CAPTURED)
+            val intentToOpenCamera = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+            intentToOpenCamera.putExtra(MediaStore.EXTRA_OUTPUT, imageUri)
+            startActivityForResult(intentToOpenCamera, IMAGE_CAPTURED)
             //}
 
         }
@@ -48,10 +60,11 @@ class MainActivity : AppCompatActivity() {
         super.onActivityResult(requestCode, resultCode, data)
         if (resultCode == RESULT_OK && requestCode == IMAGE_CAPTURED) {
             imgView?.setImageURI(imageUri)
-        }else {
+        } else {
             showAlert("Oops, Something went wrong")
         }
     }
+
 
     private fun requestCameraPermission(): Boolean {
         var permissionGranted = false
@@ -77,18 +90,50 @@ class MainActivity : AppCompatActivity() {
 
         return permissionGranted
     }
+    private fun requestGalleryPermission(): Boolean {
+        var permissionGranted = false
+
+        // If system os is Marshmallow or Above, we need to request runtime permission
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M){
+            val cameraPermissionNotGranted = checkSelfPermission( Manifest.permission.READ_EXTERNAL_STORAGE) == PackageManager.PERMISSION_DENIED
+            if (cameraPermissionNotGranted){
+                val permission = arrayOf(Manifest.permission.READ_EXTERNAL_STORAGE)
+
+                // Display permission dialog
+                requestPermissions(permission, GALLERY_PERMISSION_CODE)
+            }
+            else{
+                // Permission already granted
+                permissionGranted = true
+            }
+        }
+        else{
+            // Android version earlier than M -> no need to request permission
+            permissionGranted = true
+        }
+
+        return permissionGranted
+    }
 
     // Handle Allow or Deny response from the permission dialog
     override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
         super.onRequestPermissionsResult(requestCode, permissions, grantResults)
         if (requestCode === CAMERA_PERMISSION_CODE) {
-            if (grantResults.size === 1 && grantResults[0] == PackageManager.PERMISSION_GRANTED){
+            if (grantResults.size === 1 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
                 // Permission was granted
                 // openCameraInterface()
-            }
-            else{
+            } else {
                 // Permission was denied
                 showAlert("Camera permission was denied. Unable to take a picture.");
+            }
+            if (requestCode === GALLERY_PERMISSION_CODE) {
+                if (grantResults.size === 1 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    // Permission was granted
+                    // openCameraInterface()
+                } else {
+                    // Permission was denied
+                    showAlert("Gallery permission was denied. Unable to pick a picture.");
+                }
             }
         }
     }
